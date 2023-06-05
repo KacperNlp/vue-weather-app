0ae603bd2a0cb9f82e140535b2962e7aae1f4fd2
Kacper Nalepa <kacper.nlp@wp.pl>
2023-06-05 19:22:11 +0200

    Create app layout

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Hello there</h1>
+  <app-layout>
+    <template #main>
+      <h1>Hello main</h1>
+    </template>
+    <template #footer>
+      <p>Weather App</p>
+    </template>
+  </app-layout>
 </template>
+
+<script setup>
+import AppLayout from "./layout/AppLayout.vue";
+</script>
---
+++ src/layout/AppLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col min-h-screen">
+    <main class="w-3/4 mx-auto my-8 grow">
+      <slot name="main"></slot>
+    </main>
+    <footer class="flex justify-center bg-gray-900 px-4 py-6 text-sm">
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
