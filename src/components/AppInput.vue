4b63becb962b9198f4742551fde65a5c67977285
Kacper Nalepa <kacper.nlp@wp.pl>
2023-06-05 19:48:47 +0200

    Add styles for AppInput component

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col gap-2 mb-6">
     <label :for="inputId" class="text-gray-300 text-sm">{{ inputLabel }}</label>
-    <input type="text" :id="inputId" class="" />
+    <input
+      type="text"
+      :id="inputId"
+      class="border border-gray-400 bg-gray-700 focus:bg-gray-900 p-2 text-sm rounded-none outline-slate-500 duration-300"
+    />
   </div>
 </template>
 
