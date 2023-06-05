7db1b1706acc53b9958e6c12999fe2daeba71a5e
Kacper Nalepa <kacper.nlp@wp.pl>
2023-06-05 19:38:55 +0200

    Create AppCard component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="md:px-8 md:py-12 md:shadow-lg md:border md:border-gray-700">
+    <slot></slot>
+  </div>
+</template>
--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -1,11 +1,16 @@
 <template>
-  <form>
-    <label for="city">Type place:</label>
-    <input type="text" id="city" />
-    <app-button>Submit</app-button>
-  </form>
+  <app-card class="max-w-xl mx-auto">
+    <form>
+      <div class="flex flex-col gap-2 mb-6">
+        <label for="city">Type place:</label>
+        <input type="text" id="city" />
+      </div>
+      <app-button>Submit</app-button>
+    </form>
+  </app-card>
 </template>
 
 <script setup>
 import AppButton from "./AppButton.vue";
+import AppCard from "./AppCard.vue";
 </script>
