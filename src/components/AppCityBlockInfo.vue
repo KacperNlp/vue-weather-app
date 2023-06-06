898458aaba335fb85b18a97694ee2a49d0626179
Kacper Nalepa <kacper.nlp@wp.pl>
2023-06-06 21:38:50 +0200

    add svg-loader package

--- src/components/AppCityBlockInfo.vue
+++ src/components/AppCityBlockInfo.vue
@@ -5,19 +5,33 @@
     </h2>
     <h3 class="text-xl font-light mb-4">{{ data.location.country }}</h3>
     <ul class="flex gap-4">
-      <li>Cloud: {{ data.current.cloud }}</li>
-      <li>Temp: {{ data.current.temp_c }}</li>
-      <li>Wind: {{ data.current.wind_kph }}</li>
+      <li class="flex gap-2">
+        <app-icon :iconName="setIconForCloud" class="w-6 h-6"></app-icon>
+      </li>
+      <li class="flex gap-2">
+        <app-icon iconName="celsius" class="w-6 h-6"></app-icon>:
+        {{ data.current.temp_c }}
+      </li>
+      <li class="flex gap-2">
+        <app-icon iconName="wind" class="w-6 h-6"></app-icon>:
+        {{ data.current.wind_kph }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, computed } from "vue";
+import AppIcon from "./AppIcon.vue";
 
-defineProps({
+const props = defineProps({
   data: {
     type: Object,
   },
+});
+
+const setIconForCloud = computed(() => {
+  if (props.data.current.cloud <= 20) return "sun";
+  else return "cloud";
 });
 </script>
---
+++ src/components/AppIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+  iconName: {
+    type: String,
+    required: true,
+  },
+});
+
+const icon = defineAsyncComponent(() => import(`../svg/${props.iconName}.svg`));
+</script>
