306a760f5daa01c782936ba53cce43fd377f3186
Kacper Nalepa <kacper.nlp@wp.pl>
2023-06-12 19:42:25 +0200

    Add icons

--- src/components/AppCityBlockInfo.vue
+++ src/components/AppCityBlockInfo.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    <h2 class="text-4xl font-semibold text-indigo-500 mb-2">
+    <h2
+      class="flex items-center gap-4 text-4xl font-semibold text-indigo-500 mb-2"
+    >
       {{ data.location.name }}
+      <app-icon
+        :iconName="setIconForCloud"
+        class="w-8 h-8 fill-gray-100"
+      ></app-icon>
     </h2>
     <h3 class="text-xl font-light mb-4">{{ data.location.country }}</h3>
-    <ul class="flex gap-4">
+    <ul class="flex gap-12">
       <li class="flex gap-2">
-        <app-icon :iconName="setIconForCloud" class="w-6 h-6"></app-icon>
-      </li>
-      <li class="flex gap-2">
-        <app-icon iconName="celsius" class="w-6 h-6"></app-icon>:
+        <app-icon iconName="celsius" class="w-6 h-6"></app-icon>
         {{ data.current.temp_c }}
       </li>
       <li class="flex gap-2">
-        <app-icon iconName="wind" class="w-6 h-6"></app-icon>:
-        {{ data.current.wind_kph }}
+        <app-icon iconName="wind" class="w-6 h-6"></app-icon>
+        {{ data.current.wind_kph }} km/h
       </li>
     </ul>
   </div>
